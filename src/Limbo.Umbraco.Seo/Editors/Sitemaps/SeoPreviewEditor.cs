60783e43c50be7b2bdeed8b7b8ea36659b7007aa
Anders Bjerner <abjerner@skybrud.dk>
2022-01-06 19:08:36 +0100

    Moved SEO preview editor registration to C#

---
+++ src/Limbo.Umbraco.Seo/Editors/Sitemaps/SeoPreviewEditor.cs
@@ -0,0 +1,39 @@
+﻿using Umbraco.Cms.Core.PropertyEditors;
+
+namespace Limbo.Umbraco.Seo.Editors.Sitemaps {
+    
+    [DataEditor(EditorAlias, EditorType.PropertyValue, EditorName, EditorView,
+        ValueType = ValueTypes.String,
+        Group = "Limbo",
+        Icon = "icon-chart color-limbo")]
+    public class SeoPreviewEditor : DataEditor {
+
+        #region Constants
+
+        /// <summary>
+        /// Gets the name of the editor.
+        /// </summary>
+        public const string EditorName = "Limbo SEO Preview";
+
+        /// <summary>
+        /// Gets the alias of the editor.
+        /// </summary>
+        public const string EditorAlias = "Limbo.Umbraco.Seo.Preview";
+
+        /// <summary>
+        /// Gets the URL of the view of the editor.
+        /// </summary>
+        public const string EditorView = "/App_Plugins/Limbo.Umbraco.Seo/Views/Preview.html";
+
+        #endregion
+
+        #region Constructors
+
+        public SeoPreviewEditor(IDataValueEditorFactory dataValueEditorFactory) : base(dataValueEditorFactory) { }
+
+        #endregion
+
+    }
+
+
+}
