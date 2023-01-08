af1472007677606724a9889465dc118efb2f9190
Anders Bjerner <abjerner@skybrud.dk>
2023-01-08 18:13:16 +0100

    Cleanup

--- src/Limbo.Umbraco.Seo/App_Plugins/Limbo.Umbraco.Seo/Scripts/Controllers/SitemapChangeFrequency.js
+++ src/Limbo.Umbraco.Seo/App_Plugins/Limbo.Umbraco.Seo/Scripts/Controllers/SitemapChangeFrequency.js
@@ -18,7 +18,7 @@
     vm.frequencies.forEach(function (f) {
         f.selected = f.value === $scope.model.value;
 
-        localizationService.localize(`limboSeo_frequency_${f.value == "" ? "unspecified" : f.value}`).then(function (value) {
+        localizationService.localize(`limboSeo_frequency_${f.value === "" ? "unspecified" : f.value}`).then(function (value) {
             if (typeof value === "string" && value.length > 0 && value[0] !== "[") f.name = value;
         });
 
@@ -29,26 +29,6 @@
         vm.frequencies.forEach(function (f) {
             f.selected = f === frequency;
         });
-    };
-
-
-    var myAction = {
-        labelKey: 'general_labelForMyAction',
-        labelTokens: [],
-        icon: 'action',
-        method: function() {},
-        isDisabled: false
-    }
-
-    var propertyActions = [
-        myAction
-    ];
-
-    this.$onInit = function () {
-        console.log($scope.umbProperty);
-        if ($scope.umbProperty) {
-            $scope.umbProperty.setPropertyActions(propertyActions);
-        }
     };
 
 });
