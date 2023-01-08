3711dd5b4ad146d0cc0f8367d8a6cc47f6a2a305
Anders Bjerner <abjerner@skybrud.dk>
2023-01-08 18:41:38 +0100

    Added logging to SitemapController

--- src/Limbo.Umbraco.Seo/Sitemaps/SitemapController.cs
+++ src/Limbo.Umbraco.Seo/Sitemaps/SitemapController.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Text;
 using Umbraco.Cms.Web.Common.Controllers;
+
 #pragma warning disable CS1591
 
 namespace Limbo.Umbraco.Seo.Sitemaps {
 
     public class SitemapController : UmbracoApiController {
 
+        private readonly ILogger<SitemapController> _logger;
         private readonly ISitemapHelper _sitemapHelper;
 
-        public SitemapController(ISitemapHelper sitemapHelper) {
+        public SitemapController(ILogger<SitemapController> logger, ISitemapHelper sitemapHelper) {
+            _logger = logger;
             _sitemapHelper = sitemapHelper;
         }
 
@@ -19,6 +23,9 @@ namespace Limbo.Umbraco.Seo.Sitemaps {
 
             // Generate a new sitemap
             ISitemapResult sitemap = _sitemapHelper.BuildSitemap(HttpContext);
+
+            // Write to the log if building the sitemap failed
+            if (sitemap.Exception is not null) _logger.LogError(sitemap.Exception, "Failed building sitemap for {Domain}.", HttpContext.Request.Host);
 
             // Generate the XML for the sitemap
             StringBuilder builder = new();
