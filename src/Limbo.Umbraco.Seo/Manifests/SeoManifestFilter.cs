dc21ea2c2dc3f4191994ca6fea136b13a46b3a69
Anders Bjerner <abjerner@skybrud.dk>
2024-03-12 15:37:02 +0100

    Package manifest filter should specify package ID (Umbraco 12+)

--- src/Limbo.Umbraco.Seo/Manifests/SeoManifestFilter.cs
+++ src/Limbo.Umbraco.Seo/Manifests/SeoManifestFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using Umbraco.Cms.Core.Manifest;
 
 namespace Limbo.Umbraco.Seo.Manifests {
@@ -8,7 +9,9 @@ namespace Limbo.Umbraco.Seo.Manifests {
 
         /// <inheritdoc />
         public void Filter(List<PackageManifest> manifests) {
-            manifests.Add(new PackageManifest {
+
+            // Initialize a new manifest filter for this package
+            PackageManifest manifest = new() {
                 AllowPackageTelemetry = true,
                 PackageName = SeoPackage.Name,
                 Version = SeoPackage.InformationalVersion,
@@ -21,7 +24,21 @@ namespace Limbo.Umbraco.Seo.Manifests {
                 Stylesheets = new[] {
                     $"/App_Plugins/{SeoPackage.Alias}/Styles/Styles.css"
                 }
-            });
+            };
+
+            // The "PackageId" property isn't available prior to Umbraco 12, and since the package is build against
+            // Umbraco 10, we need to use reflection for setting the property value for Umbraco 12+. Ideally this
+            // shouldn't fail, but we might at least add a try/catch to be sure
+            try {
+                PropertyInfo? property = manifest.GetType().GetProperty("PackageId");
+                property?.SetValue(manifest, SeoPackage.Alias);
+            } catch {
+                // We don't really care about the exception
+            }
+
+            // Append the manifest
+            manifests.Add(manifest);
+
         }
 
     }
